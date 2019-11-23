433b7f149c39e3e60010e67dd836770d73f89a8d
Edward Wibowo <43205692+claby2@users.noreply.github.com>
2019-11-23 14:43:42 +0800

    Added Speech Recognition
    Compare function not finished

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,8 +1,17 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+var speechWords = [];
+
 let textInput = document.getElementById("textinput");
 let studyButton = document.getElementById("studybutton");
 let resultDiv = document.getElementById("resultdiv");
+let recordButton = document.getElementById("recordbutton");
 
 let init = false;
+
+let isRecording = false;
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -21,6 +30,26 @@ studyButton.addEventListener("click", ()=>{ //Button Clicked
         textInput.value = "";
     
         arr = text.split(" ");
+
+        speechWords = arr.slice()
+            .reduce((p, n) => 
+                p.concat(
+                    n.replace(/[^\w\s]/gm, ' ')
+                    .split(/\s+/gm)
+                    .filter(e=>e)
+                ), 
+                []
+            );
+        var grammar = '#JSGF V1.0; grammar speechWords; public <word> = ' + speechWords.join(' | ') + ' ;';
+        var recognition = new SpeechRecognition();
+        var speechRecognitionList = new SpeechGrammarList();
+        speechRecognitionList.addFromString(grammar, 1);
+
+        recognition.grammars = speechRecognitionList;
+
+        recognition.lang = 'en-US';
+        recognition.interimResults = false;
+        recognition.maxAlternatives = 1;
 
         for(let i = 0; i < arr.length; i++){
             val.push(i);
@@ -59,3 +88,63 @@ studyButton.addEventListener("click", ()=>{ //Button Clicked
 
     }
 });
+
+var grammar = '#JSGF V1.0; grammar speechWords; public <word> = ' + speechWords.join(' | ') + ' ;';
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+
+recognition.grammars = speechRecognitionList;
+
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+recordButton.addEventListener("click", ()=>{
+    if(init && !isRecording){
+        isRecording = true;
+        console.log("record");
+        recognition.start();
+    } else if(isRecording){
+        console.log("record done")
+        recognition.stop();
+        isRecording = false;
+    }
+});
+
+var speech = [];
+
+recognition.onresult = function(event) {
+    let result = event.results[event.resultIndex]
+    if (result.isFinal) {
+        speech = result[0].transcript;
+        let speechSplit = [];
+        let temp;
+        for(let i = 0; i < speech.length; i++){
+            if(speech[i] == " "){
+                speechSplit.push(temp);
+                temp = "";
+            } else{
+                temp = temp + speech[i];
+            }
+        }
+        console.log(compare(speechSplit, speechWords) + "%");
+    }
+
+}
+
+function compare(arr1, arr2) {
+    let total = Math.max(arr1.length, arr2.length);
+    let amount = 0;
+    
+    for(let i = 0; i < Math.min(arr1.length, arr2.length); i++){
+        total = total + 1;
+        if(arr1[i].toLowerCase() == arr2[i].toLowerCase()){
+            amount = amount + 1;
+        }
+
+        console.log(arr1[i] + " " + arr2[i]);
+    }
+
+    return (amount/total)*100;
+}
